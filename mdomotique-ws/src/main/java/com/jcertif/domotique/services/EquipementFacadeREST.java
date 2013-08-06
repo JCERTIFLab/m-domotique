300dd7472e45f0695dbbd37d01d3e4dfb76de1fa
Firas8 <firas.gabsi@gmail.com>
2013-08-07 00:50:11 +0200

    Ajout de java Embdded dans le web service

--- mdomotique-ws/src/main/java/com/jcertif/domotique/services/EquipementFacadeREST.java
+++ mdomotique-ws/src/main/java/com/jcertif/domotique/services/EquipementFacadeREST.java
@@ -5,6 +5,8 @@
 package com.jcertif.domotique.services;
 
 import com.jcertif.domotique.entities.Equipement;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
@@ -46,7 +48,7 @@ public class EquipementFacadeREST extends AbstractFacade<Equipement> {
 
     @GET
     @Path("{id}")
-    @Produces({ "application/json"})
+    @Produces({"application/json"})
     public Equipement find(@PathParam("id") Integer id) {
         return super.find(id);
     }
@@ -61,7 +63,7 @@ public class EquipementFacadeREST extends AbstractFacade<Equipement> {
 
     @GET
     @Path("{from}/{to}")
-    @Produces({ "application/json"})
+    @Produces({"application/json"})
     public List<Equipement> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
@@ -73,21 +75,76 @@ public class EquipementFacadeREST extends AbstractFacade<Equipement> {
         return String.valueOf(super.count());
     }
 
-    
     @POST
     @Path("getEquipementsByRoom/{piece_id}")
     @Produces({"application/json"})
     public List<Equipement> findByPiece(@PathParam("piece_id") Integer piece_id) {
-       javax.persistence.Query cq = getEntityManager().createNamedQuery("Equipement.findByPiece").setParameter("piece_id", piece_id);
-        return (List<Equipement>)cq.getResultList();
+        javax.persistence.Query cq = getEntityManager().createNamedQuery("Equipement.findByPiece").setParameter("piece_id", piece_id);
+        return (List<Equipement>) cq.getResultList();
     }
-    
+
     @POST
     @Path("action/{id}/{etat}")
     @Produces({"application/json"})
     public void edit(@PathParam("id") Integer id, @PathParam("etat") String etat) {
+        
         Equipement equipement = super.find(id);
-        equipement.setEtat(Boolean.valueOf(etat));
-        super.edit(equipement);
+        if(action(equipement.getRelay(), etat)){
+            equipement.setEtat(Boolean.valueOf(etat));
+            super.edit(equipement);
+        }
+    }
+
+    public boolean action(String GpioChannels, String state) {
+       
+        final String GPIO_OUT = "out";
+        final String GPIO_ON = "1";
+        final String GPIO_OFF = "0";
+        
+        boolean result = false;
+    
+        try {
+            // Open file handles to GPIO port unexport and export controls  
+            FileWriter unexportFile = new FileWriter("/sys/class/gpio/unexport");
+            FileWriter exportFile = new FileWriter("/sys/class/gpio/export");
+
+            // Loop through all ports if more than 1  
+            // Reset the port, if needed  
+            File exportFileCheck = new File("/sys/class/gpio/gpio" + GpioChannels);
+            if (exportFileCheck.exists()) {
+                unexportFile.write(GpioChannels);
+                unexportFile.flush();
+            }
+
+            // Set the port for use  
+            exportFile.write(GpioChannels);
+            exportFile.flush();
+
+            // Open file handle to port input/output control  
+            FileWriter directionFile = new FileWriter("/sys/class/gpio/gpio" + GpioChannels + "/direction");
+
+            // Set port for output  
+            directionFile.write(GPIO_OUT);
+            directionFile.flush();
+            // Set up a GPIO port as a command channel  
+            FileWriter commandChannel = new FileWriter("/sys/class/gpio/gpio" + GpioChannels + "/value");
+            
+            if(state.equals("0")){
+                // HIGH: Set GPIO port ON  
+                commandChannel.write(GPIO_ON);
+                commandChannel.flush();
+            }else{
+                // LOW: Set GPIO port OFF  
+                commandChannel.write(GPIO_OFF);
+                commandChannel.flush();
+            }
+            
+            result = true;
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        
+        return result;
     }
 }
