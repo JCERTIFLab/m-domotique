ef12fdaeff89be4ec61d6a68025cd1d224c264e2
Firas8 <firas.gabsi@gmail.com>
2013-08-09 19:08:04 +0200

    Revert "Suppression de mdomotique-desktop"
    This reverts commit f8f565df31224ed4f9d2a3c25ea35fcf34efe4e0.

---
+++ mdomotique-desktop/src/main/java/com/jcertif/mdomotique/desktop/MDomotiqueApplication.java
@@ -0,0 +1,37 @@
+package com.jcertif.mdomotique.desktop;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MDomotiqueApplication extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Bienvenu dans M-Domotique");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
